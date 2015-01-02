3ca35a400bbcbabe25f58c78ccb8d7a7c1b066bb
Uice Lu <uicestone@gmail.com>
2015-01-03 03:32:30 +0800

    优化公交线路更新脚本

--- app/commands/UpdateLines.php
+++ app/commands/UpdateLines.php
@@ -55,15 +55,16 @@ class UpdateLines extends Command {
 		
 		$url = Config::get('shjtmap.' . $region . '.' . $api_name) . '?' . http_build_query($query_args);
 		
-		$this->info('Calling: ' . $url);
+//		$this->info('Calling: ' . $url);
 		
-		$xml = simplexml_load_file($url);
+		$xml = file_get_contents($url);
 		
-		$object = $this->parseXml($xml, $as_array);
+		$simpleXmlObject = simplexml_load_string($xml);
 		
-		$this->info('Object parsed as:');
+		$object = $this->parseXml($simpleXmlObject, $as_array);
 		
-		var_export($object); echo "\n";
+//		$this->info('Object parsed as:');
+//		var_export($object); echo "\n";
 		
 		return $object;
 	}
@@ -80,73 +81,84 @@ class UpdateLines extends Command {
 		
 		foreach($lines as $line)
 		{
-			$line_base = $line['@attributes'];
 			
-			$query_args = $this->option('region') === 'px' ? array('name'=>$line_base['actual']) : array('linename'=>$line_base['name']);
+			try{
 			
-			$line_detail = $this->getFromApi('get_line_info_by_name', $this->option('region'), $query_args);
-			
-			if($this->option('region') === 'pd')
-			{
-				$line_detail = $line_detail->linedetail;
-			}
-			
-			$directions = (array) $this->getFromApi('get_line', $this->option('region'), array('lineid'=>$line_detail->line_id));
-			
-			foreach($directions as $direction)
-			{
-				$direction->direction = $direction->direction === 'true';
-				
-				$line = Line::firstOrCreate(array(
-					'slug'=>$line_detail->line_name,
-					'direction'=>$direction->direction
-				));
-				
-				$origin_stop = Stop::firstOrCreate(array('name'=>$line_detail->start_stop));
-				$terminal_stop = Stop::firstOrCreate(array('name'=>$line_detail->end_stop));
+				$line_base = $line['@attributes'];
 
-				$line->fill(array(
-					'name'=>$line_base['name'],
-					'line_id'=>$line_detail->line_id,
-					'first_vehicle_hour'=>$direction->direction ? $line_detail->start_earlytime : $line_detail->end_earlytime,
-					'last_vehicle_hour'=>$direction->direction ? $line_detail->start_latetime : $line_detail->end_latetime
-				));
-				
-				$line->originStop()->associate($direction->direction ? $origin_stop : $terminal_stop);
-				$line->terminalStop()->associate($direction->direction ? $terminal_stop : $origin_stop);
+				$query_args = $this->option('region') === 'px' ? array('name'=>$line_base['actual']) : array('linename'=>$line_base['name']);
 
-				$line->save();
-				
-				if(!property_exists($direction, 'stop'))
+				if(Line::where('name', $line_base['name'])->get()->count() === 2)
 				{
 					continue;
 				}
 				
-				foreach($direction->stop as $stop_raw)
+				$line_detail = $this->getFromApi('get_line_info_by_name', $this->option('region'), $query_args);
+
+				if($this->option('region') === 'pd')
 				{
-					$stop = Stop::firstOrCreate(array(
-						'name'=>$stop_raw->zdmc,
+					$line_detail = $line_detail->linedetail;
+				}
+
+				$directions = (array) $this->getFromApi('get_line', $this->option('region'), array('lineid'=>$line_detail->line_id));
+
+				foreach($directions as $direction)
+				{
+					$direction->direction = $direction->direction === 'true';
+
+					$line = Line::firstOrCreate(array(
+						'slug'=>$line_detail->line_name,
+						'direction'=>$direction->direction
 					));
-					
-					try
+
+					$origin_stop = Stop::firstOrCreate(array('name'=>$line_detail->start_stop));
+					$terminal_stop = Stop::firstOrCreate(array('name'=>$line_detail->end_stop));
+
+					$line->fill(array(
+						'name'=>$line_base['name'],
+						'line_id'=>$line_detail->line_id,
+						'first_vehicle_hour'=>$direction->direction ? $line_detail->start_earlytime : $line_detail->end_earlytime,
+						'last_vehicle_hour'=>$direction->direction ? $line_detail->start_latetime : $line_detail->end_latetime
+					));
+
+					$line->originStop()->associate($direction->direction ? $origin_stop : $terminal_stop);
+					$line->terminalStop()->associate($direction->direction ? $terminal_stop : $origin_stop);
+
+					$line->save();
+
+					if(!property_exists($direction, 'stop'))
 					{
-						$line->stops()->attach($stop->id, array('stop_no'=>$stop_raw->id));
+						continue;
 					}
-					catch(PDOException $e)
+
+					foreach($direction->stop as $stop_raw)
 					{
-						// A "Dupulicate Entry" exception is considered normal here
-						if(strpos($e->getMessage(), 'Duplicate entry') === false)
+						$stop = Stop::firstOrCreate(array(
+							'name'=>$stop_raw->zdmc,
+						));
+
+						try
 						{
-							throw new PDOException($e->getMessage(), $e->getCode(), $e);
+							$line->stops()->attach($stop->id, array('stop_no'=>$stop_raw->id));
+						}
+						catch(PDOException $e)
+						{
+							// A "Dupulicate Entry" exception is considered normal here
+							if(strpos($e->getMessage(), 'Duplicate entry') === false)
+							{
+								throw new PDOException($e->getMessage(), $e->getCode(), $e);
+							}
 						}
 					}
+
+					Log::info($line['name'] . ' saved.');
+					$this->info($line['name'] . ' saved.');
 				}
 			}
-			
-			Log::info($line['name'] . ' saved.');
-			$this->info($line['name'] . ' saved.');
-			sleep(rand(0, 2));
-			
+			catch(Exception $e){
+				$this->error($e->getMessage());
+				Log::error($e->getMessage());
+			}
 		}
 		
 	}
