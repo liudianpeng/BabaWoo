d98757cd67bb36a1ac475fddfa8fc558cc76b42f
Uice Lu <uicestone@gmail.com>
2015-01-03 01:56:11 +0800

    接口配置文件模版

---
+++ app/config/shjtmap.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+	'pd'=>array(
+		'key'=>'',
+		'get_line_info_by_name'=>'',
+		'get_line'=>'',
+		'car_monitor'=>'',
+		'line'=>''
+	),
+	'px'=>array(
+		'key'=>'',
+		'get_line_info_by_name'=>'',
+		'get_line'=>'',
+		'car_monitor'=>'',
+		'get_dispatch_screen'=>'',
+		'line'=>''
+	)
+);
