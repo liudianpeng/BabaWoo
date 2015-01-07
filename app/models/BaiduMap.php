812001c99fccbd774f3c34d0136715bf97078fd6
Uice Lu <uicestone@gmail.com>
2015-01-07 11:20:41 +0800

    百度API下沉

--- app/models/BaiduMap.php
+++ app/models/BaiduMap.php
@@ -2,6 +2,22 @@
 
 class BaiduMap {
 	
+	public static function getPlaces($location_name, $region = '上海市')
+	{
+		$query_args = array(
+			'query'=>$location_name,
+			'region'=>$region,
+			'output'=>'json',
+			'ak'=>Config::get('baidumap.ak')
+		);
+		
+		$url = 'http://api.map.baidu.com/place/v2/search?' . http_build_query($query_args);
+		
+		$result = file_get_contents($url);
+		
+		return json_decode($result);
+	}
+
 	public static function geoConv($latlng)
 	{
 		$url_geoconv = 'http://api.map.baidu.com/geoconv/v1/?';
